5303808b39565d283ed42516810eb5289f112840
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-10 23:06:02 +0000

    create methods to do parallelism

--- lib/reports_hours.ex
+++ lib/reports_hours.ex
@@ -45,6 +45,38 @@ defmodule ReportsHours do
     |> Enum.reduce(report_head(), fn line, report -> sum_values(line, report) end)
   end
 
+  def build_many_csv(file_names) when not is_list(file_names) do
+    {:error, "Please provice a list from files"}
+  end
+
+  def build_many_csv(file_names) do
+    result = file_names
+             |> Task.async_stream(&build/1)
+             |> Enum.reduce(report_head(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+    {:ok, result}
+  end
+
+  defp sum_reports(%{
+         "all_hours" => hours1,
+         "hours_per_month" => months1,
+         "hours_per_year" => years1
+       }, %{
+         "all_hours" => hours2,
+         "hours_per_month" => months2,
+         "hours_per_year" => years2
+       }) do
+         hours = merge_maps(hours1, hours2)
+         months = merge_maps(months1, months2)
+         years = merge_maps(years1, years2)
+
+         build_report(hours, months, years)
+            end
+
+  defp merge_maps(map1, map2) do
+    Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+  end
+
   defp sum_values([person, hour, _day, month, year], %{
          "all_hours" => hours,
          "hours_per_month" => months,
