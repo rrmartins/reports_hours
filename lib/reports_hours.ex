db03996236fdfdbbc05ced5228806019c28ff66b
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-10 19:19:52 +0000

    start the project

---
+++ lib/reports_hours.ex
@@ -0,0 +1,18 @@
+defmodule ReportsHours do
+  @moduledoc """
+  Documentation for `ReportsHours`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ReportsHours.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
