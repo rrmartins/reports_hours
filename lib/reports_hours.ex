0a7727d003a07d306e01ef6e894b08c04a985765
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-14 22:06:00 +0000

    change where call report_head method to Head module

--- lib/reports_hours.ex
+++ lib/reports_hours.ex
@@ -1,48 +1,10 @@
 defmodule ReportsHours do
-  alias ReportsHours.{Build, Parser}
-
-  @available_person [
-    :rodrigo,
-    :natali,
-    :rafael,
-    :diego,
-    :daniele,
-    :mayk,
-    :jakeliny,
-    :giuliano,
-    :joseph,
-    :cleiton,
-    :vinicius,
-    :danilo
-  ]
-
-  @months [
-    :jan,
-    :feb,
-    :mar,
-    :apr,
-    :may,
-    :jun,
-    :jul,
-    :aug,
-    :sep,
-    :oct,
-    :nov,
-    :dec
-  ]
-
-  @years [
-    "2016",
-    "2017",
-    "2018",
-    "2019",
-    "2020"
-  ]
+  alias ReportsHours.{Build, Head, Parser}
 
   def build(file_name) do
     file_name
     |> Parser.build()
-    |> Enum.reduce(report_head(), fn line, report -> sum_values(line, report) end)
+    |> Enum.reduce(Head.call(), fn line, report -> sum_values(line, report) end)
   end
 
   def build_many_csv(file_names) when not is_list(file_names) do
@@ -53,7 +15,7 @@ defmodule ReportsHours do
     result =
       file_names
       |> Task.async_stream(&build/1)
-      |> Enum.reduce(report_head(), fn {:ok, result}, report -> sum_reports(report, result) end)
+      |> Enum.reduce(Head.call(), fn {:ok, result}, report -> sum_reports(report, result) end)
 
     {:ok, result}
   end
@@ -109,15 +71,5 @@ defmodule ReportsHours do
     years = Map.put(years, person, year)
 
     Build.call(hours, months, years)
-  end
-
-  defp report_head do
-    all_hours = Enum.into(@available_person, %{}, &{&1, 0})
-    months = Enum.into(@months, %{}, &{&1, 0})
-    hours_per_month = Enum.into(@available_person, %{}, &{&1, months})
-    years = Enum.into(@years, %{}, &{&1, 0})
-    hours_per_year = Enum.into(@available_person, %{}, &{&1, years})
-
-    Build.call(all_hours, hours_per_month, hours_per_year)
   end
 end
